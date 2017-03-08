addcf37e6df22a80b9fef22db44222419451fd81
msa455 <msa455@nyu.edu>
2017-03-08 13:22:00 -0500

    Add files via upload
    The site I created for hackshanghai 2015

---
+++ Hackathon Site/js/translation.js
@@ -0,0 +1,10 @@
+//initialize dictionary of text, site_text = {}, first slot as english, second slot as chinese
+
+//check if flag is english (0) or chinese (1)
+//if flag is 0, set flag to 1
+//else set flag to 0
+
+//iterate through every element in class text
+//use innerhtml to reassign each value ex: element.innerHTML = site_text.i[1]
+
+//do the reverse if chinese to english
